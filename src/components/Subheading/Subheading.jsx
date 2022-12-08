7f0c18068a4894432281cd8342e5bfb40492b5a0
Saumya Sharma <saumyasharma@Saumyas-MacBook-Pro-3.local>
2022-12-08 15:55:01 -0500

    added header section

--- src/components/Subheading/Subheading.jsx
+++ src/components/Subheading/Subheading.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./subheading.css";
+import { images } from "../../constants";
 
-const Subheading = () => {
-  return <div>Subheading</div>;
+const Subheading = ({ title }) => {
+  return (
+    <div className="g__subheading p__cormorant">
+      <p>{title}</p>
+      <img src={images.spoon} alt={title} />
+    </div>
+  );
 };
 
 export default Subheading;
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./header.css";
+import { images } from "../../constants";
+import { Subheading } from "../../components";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="g__header section__padding g__wrapper" id="home">
+      <div className="g__wrapper-info">
+        <Subheading title="Chase the new Flavour" />
+        <h1 className="g__header-h1">The key to Fine dining</h1>
+        <p className="p__opensans g__header-p">
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+          volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+          aliquam amet tellus
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
+      </div>
+      <div className="g__wrapper-img">
+        <img src={images.welcome} alt="Welcome" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
