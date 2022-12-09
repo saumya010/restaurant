bf20e1254eac01889d5f328ab2fd286b6e859582
Saumya Sharma <saumyasharma@Saumyas-MacBook-Pro-3.local>
2022-12-09 13:36:45 -0500

    added menu section

--- src/components/Menuitem/Menuitem.jsx
+++ src/components/Menuitem/Menuitem.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./menuitem.css";
 
-const Menuitem = () => {
-  return <div>Menuitem</div>;
+const Menuitem = ({ title, price, tags }) => {
+  return (
+    <div className="g__menuitem">
+      <div className="g__menuitem-head">
+        <div className="g__menuitem-name">
+          <p className="p__cormorant" style={{ color: "#dcca87" }}>
+            {title}
+          </p>
+        </div>
+        <div className="g__menuitem-dash"></div>
+        <div className="g__menuitem-price">
+          <p className="p__cormorant">{price}</p>
+        </div>
+      </div>
+      <div className="g__menuitem-tags">
+        <p className="p__opensans" style={{ color: "#aaa" }}>
+          {tags}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Menuitem;
--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import "./menu.css";
+import { images, data } from "../../constants";
+import { Subheading, Menuitem } from "../../components";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="g__menu flex__center section__padding" id="menu">
+      <div className="g__menu-heading">
+        <Subheading title="Menu that fits your palate" />
+        <h2 className="headtext__cormorant">Today's Special</h2>
+      </div>
+      <div className="g__menu-content">
+        <div className="g__menu-content_left">
+          <p className="g__menu-content_heading">Wine & Beer</p>
+          <div className="g__menu-content_items">
+            {data.wines.map((wine, index) => {
+              return (
+                <Menuitem
+                  key={wine.title + index}
+                  title={wine.title}
+                  price={wine.price}
+                  tags={wine.tags}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="g__menu-content_center">
+          <img src={images.menu} alt="Menu" />
+        </div>
+        <div className="g__menu-content_right">
+          <p className="g__menu-content_heading">Cocktails</p>
+          <div className="g__menu-content_items">
+            {data.cocktails.map((cocktail, index) => {
+              return (
+                <Menuitem
+                  key={cocktail.title + index}
+                  title={cocktail.title}
+                  price={cocktail.price}
+                  tags={cocktail.tags}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="g__menu-button">
+        <button className="custom__button" type="button">
+          View More
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
